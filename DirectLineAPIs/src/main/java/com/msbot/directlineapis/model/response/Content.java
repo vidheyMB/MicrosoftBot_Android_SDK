bd109582a0b014213e2a3139d8ad6382ce1e1476
arokiait_mobile <manoj@in.arokiait.com>
2020-10-22 19:47:27 +0530

    Value changed to Object from String

--- DirectLineAPIs/src/main/java/com/msbot/directlineapis/model/response/Content.java
+++ DirectLineAPIs/src/main/java/com/msbot/directlineapis/model/response/Content.java
@@ -19,13 +19,13 @@ public class Content {
     @Json(name = "lgtype")
     private String lgtype;
     @Json(name = "value")
-    private String value;
+    private Object value;
 
-    public String getValue() {
+    public Object getValue() {
         return value;
     }
 
-    public void setValue(String value) {
+    public void setValue(Object value) {
         this.value = value;
     }
 
